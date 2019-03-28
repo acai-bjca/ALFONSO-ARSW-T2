6165f502acc34e3cda9c7c7da71e83c72b03ce65
karenMora <karen.mora@mail.escuelaing.edu.co>
2019-03-28 16:23:32 -0500

    iniciando proyecto

---
+++ src/main/java/edu/eci/arsw/ALFONSOARSWT2/AlfonsoArswT2Application.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.ALFONSOARSWT2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfonsoArswT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfonsoArswT2Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/ALFONSOARSWT2/AlfonsoArswT2ApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.eci.arsw.ALFONSOARSWT2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlfonsoArswT2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
